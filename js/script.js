e5dd8aae9f912eee13120df4d0b57adbca4adc08
Alexey <mandzhievav@gmail.com>
2017-07-12 01:26:11 +0300

    Поправка слайдера

--- js/script.js
+++ js/script.js
@@ -1,26 +1,62 @@
-$(document).ready(function() {
+(function () {
+    var tab1 = document.querySelector(".tab_item__vibe");
+    var tab2 = document.querySelector(".tab_item__yoga");
+    var tab3 = document.querySelector(".tab_item__ideapad");
+    var tab4 = document.querySelector(".tab_item__a-series");
+    var slide1 = document.querySelector(".slideshow_item__vibe");
+    var slide2 = document.querySelector(".slideshow_item__yoga");
+    var slide3 = document.querySelector(".slideshow_item__ideapad");
+    var slide4 = document.querySelector(".slideshow_item__a-series");
 
-	(function () {
-        $('.tab_link').on('click', function(e){
-		e.preventDefault();
-		var
-			$this = $(this),
-			item = $this.closest('.tab_item'),
-			container = $this.closest('.main'),
-			content = container.find('.slideshow_item'),
-			ndx = item.index(),
-			reqItem = content.eq(ndx),
-			activeItem = content.filter('.slideshow_item__active');
-			item.addClass('tab_item__active')
-				.siblings()
-				.removeClass('tab_item__active');
-			activeItem.fadeOut(100, function () {
-				reqItem.fadeIn(100, function () {
-					$(this).addClass('slideshow_item__active')
-						.siblings()
-						.removeClass('slideshow_item__active');
-				});
-			});
-		});
-    })();
-});
+    tab1.addEventListener("click", function(event) {
+        event.preventDefault();
+        console.log("1");
+        tab2.classList.remove('tab_item__active');
+        tab3.classList.remove('tab_item__active');
+        tab4.classList.remove('tab_item__active');  
+        tab1.classList.add('tab_item__active'); 
+        slide2.classList.remove('slideshow_item__active');
+        slide3.classList.remove('slideshow_item__active');
+        slide4.classList.remove('slideshow_item__active');  
+        slide1.classList.add('slideshow_item__active');  
+    });
+
+    tab2.addEventListener("click", function(event) {
+        event.preventDefault();
+        console.log("2");
+        tab1.classList.remove('tab_item__active');
+        tab3.classList.remove('tab_item__active');
+        tab4.classList.remove('tab_item__active');  
+        tab2.classList.add('tab_item__active');
+        slide1.classList.remove('slideshow_item__active');
+        slide3.classList.remove('slideshow_item__active');
+        slide4.classList.remove('slideshow_item__active');  
+        slide2.classList.add('slideshow_item__active');        
+    });
+
+    tab3.addEventListener("click", function(event) {
+        event.preventDefault();
+        console.log("3");
+        tab2.classList.remove('tab_item__active');
+        tab1.classList.remove('tab_item__active');
+        tab4.classList.remove('tab_item__active');  
+        tab3.classList.add('tab_item__active');
+        slide2.classList.remove('slideshow_item__active');
+        slide1.classList.remove('slideshow_item__active');
+        slide4.classList.remove('slideshow_item__active');  
+        slide3.classList.add('slideshow_item__active');                                
+    });
+
+    tab4.addEventListener("click", function(event) {
+        event.preventDefault();
+        console.log("4");
+        tab2.classList.remove('tab_item__active');
+        tab3.classList.remove('tab_item__active');
+        tab1.classList.remove('tab_item__active');  
+        tab4.classList.add('tab_item__active');
+        slide2.classList.remove('slideshow_item__active');
+        slide3.classList.remove('slideshow_item__active');
+        slide1.classList.remove('slideshow_item__active');  
+        slide4.classList.add('slideshow_item__active');                                
+    });
+})();
